a50fe4f526481f2391b5364ddcc4764bec13758a
avell21 <abeltyr@gmail.com>
2021-07-25 03:44:30 +0300

    (feat) setup prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ApolloServer } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
 import api from "./api";
+const prisma = new PrismaClient();
 
 const apolloServer = new ApolloServer(api);
 apolloServer.listen().then(({ url }) => {
